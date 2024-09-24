8ef772e2cbea7848dde77aff600a989cd1d6ff78
Jessica Orihuela Rojas <jessica.orihuela.rojas@gmail.com>
2024-09-24 04:40:34 -0400

    Añadiendo botón de validación de contrasena

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let campoContrasena = document.getElementById('contrasena');
+let campoValidacion = document.getElementById('tipoContrasena');
 
 function generarContrasena(){
     let cantidadCaracteres = document.getElementById('cantidad').value;
-    let caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
 
     if (cantidadCaracteres != "" ){
         contrasena = '';
@@ -10,6 +11,7 @@ function generarContrasena(){
             contrasena = contrasena + caracteres[posicionRandom(caracteres.length - 1)];
         }
         campoContrasena.value = contrasena;
+        validarContrasena(contrasena);
     } else {
         alert('Debe ingresar la cantidad de caracteres');
     }
@@ -21,4 +23,38 @@ function posicionRandom(max){
 
 function limpiarCampo(){
     campoContrasena.value = '';
+    campoValidacion.innerHTML = '';
+}
+
+// Verificando contrasena
+
+function validarContrasena(contrasena){
+    if(verificarMayusculas(contrasena) && verificarNumeros(contrasena)){
+        campoValidacion.innerHTML = 'La contraseña es fuerte';
+        campoValidacion.style = "color: #27ec42";
+    } else {
+        campoValidacion.innerHTML = 'La contraseña es débil';
+        campoValidacion.style = "color: red";
+    }
+}
+
+function verificarMayusculas(texto){
+    const letrasMayusculas = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ];
+    for (let char of letrasMayusculas){
+        if(texto.includes(char)){
+            return true;
+        }
+    }
+}
+
+function verificarNumeros(texto){
+    const numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    for (let char of numeros){
+        if(texto.includes(char)){
+            return true;
+        }
+    }
 }
